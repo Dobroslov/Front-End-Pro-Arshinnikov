79dc9ab2e9ced7a2c24376d259ccf5b9acd87ea5
Vladimir <78982910+Dobroslov@users.noreply.github.com>
2021-04-24 16:37:38 +0300

    add dz_1 and dz_2 whith using script

---
+++ DZ_2_tipe_variable/script.js
@@ -0,0 +1,9 @@
+let greeting = 'Здарова';
+let user_answer = prompt('Как звать братюня?');
+alert(greeting + ', ' + user_answer + '! ' + 'Чё как?');
+
+
+        // Это нормальный образец, то что выше это тоже самое, но с элементами юмора
+// let greeting = 'Hello';
+// let user_answer = prompt('What is your name?');
+// alert(greeting + ', ' + user_answer + '! ' + 'How are you?');
