9a144e21883ea66f4a34e78d45650388d308b1dc
Vladimir <78982910+Dobroslov@users.noreply.github.com>
2021-05-17 16:01:36 +0300

    end DZ_6

---
+++ DZ_6_multi_calculator_massiv/script.js
@@ -0,0 +1,71 @@
+function getOperator () {
+
+    let answer;
+
+    do {
+            answer = prompt('Введи необходимое арифметическое действие (Варианты: "+" "-" "*" "/"');
+    } while (!isOperatorValid(answer))
+    return answer;
+}
+
+function isOperatorValid(str) {
+  return str === "+" || 
+         str === "-" || 
+         str === "*" || 
+         str === "/"
+}
+
+function getArrayOfValidValue() {
+
+  let arrValue = '';
+
+  do {
+    answer = prompt('Введи все числа для арифметического действия через запятую'); 
+  } while (answer === null || answer === '')
+    arrValue = answer.split(',');
+
+    return filterNumbers = arrValue.filter(function(index) {
+            return (!isNaN(index));
+          }
+    )
+}
+
+
+function getArrayNumbers(array) {  
+  return array.map(Number);
+}
+
+function getResult (operator) {
+  
+  sum = arrayNumbers[0];
+
+  for(let i = 1; i < (arrayNumbers.length); i++) {
+
+    switch(operator) {
+      case '+':
+        sum = sum + arrayNumbers[i];  
+        console.log(sum);    
+        break;
+      case '-':
+        sum = sum - arrayNumbers[i];
+        break;
+      case '*':
+        sum = sum * arrayNumbers[i];
+        break;
+      case '/':
+        sum = sum / arrayNumbers[i];
+        break;
+    }    
+  }
+  return sum;
+}
+
+const operator = getOperator();
+let arrayOfValidValue = getArrayOfValidValue();
+let arrayNumbers = getArrayNumbers (arrayOfValidValue);
+let result = getResult();
+alert(`Результат: ${arrayNumbers.join(operator)} = ${result}`);
+
+
+
+
