62b028a7c514e40743d4722fca1586dddd39dfa6
Vladimir <78982910+Dobroslov@users.noreply.github.com>
2021-05-29 18:09:13 +0300

    end DZ 10

---
+++ DZ_10_DOM_hellow/script.js
@@ -0,0 +1,20 @@
+let userName = getUserName()
+
+function getUserName() {
+  let answerUser = '';
+
+  do {
+    answerUser = prompt('Введи своё имя', 'Александр');
+  } while (isValidUserName(answerUser)); // пока будет true будет обновляться цикл
+
+  return answerUser;
+}
+
+function isValidUserName(message) {
+  return (message === '' || message === null); // проверка валидности значения
+}
+
+document.getElementById('user_name').innerHTML = userName + ', это было интересное задание!'; // на странице нахожу по id="user_name" и через innerHTML приваиваю span переменную с именем и добавленной строкой
+
+
+// document.getElementsByTagName('h1')[0].innerHTML = userName + ', это было интересное задание!';
