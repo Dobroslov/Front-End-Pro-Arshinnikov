04fb0e764ff9d48660b6d490a391266ff7a17dcf
Vladimir <78982910+Dobroslov@users.noreply.github.com>
2021-06-04 14:09:33 +0300

    complet dz

--- DZ_11_to_do_list/script.js
+++ DZ_11_to_do_list/script.js
@@ -1,6 +1,5 @@
 // На странице находится инпут и кнопка. Список изначально пустой
 
-
 // Пользователь может ввести что-то в инпут и нажать на кнопку, после этого в списке выше должна отобразится строка с тем что было введено в инпуте. Инпут очищается.
 
 // если пользователь нажал добавить, но содержимое пустое, то ничего не делать.
@@ -8,36 +7,24 @@
 // в каждом элементе справа должны быть кнопочка удалить. Сложный элемент
 
 const listContainer = document.getElementById('listContainer'); // вызов списка ul
-const buttonSubmitTask =  document.getElementById('saveTask'); // кнопка сабмит
+
 const inputText = document.getElementById('inputText'); // вызов текстового поля инпута
+
+const buttonSubmitTask =  document.getElementById('saveTask'); // вызов кнопки сабмит
+
 const template = `
 <li class="task-performed">
-    <span class="task-text" id="textTask"></span>
-    <button class="button-delet-task">Delete</button>
+    <span class="task-text">$CONTENT$</span>    
+    <button class="button-delete-task">Delete</button>
 </li> 
 `; // переменная в которой содержится html код для лишки
 
-buttonSubmitTask.addEventListener('click', clickHandler); // получаем элемент button в него добавляем список событий с событием 'click'(одинарное нажатие кнопки мыши) и запускаем функцию clickHandler
-inputText.addEventListener('change', clickHandler); // получаем элемент input в него добавляем список событий с событием 'change' (которое будет валидаций для пустого ввода?) (одинарное нажатие кнопки мыши) и запускаем функцию clickHandler
+buttonSubmitTask.addEventListener('click', createNewTask); // получаем элемент button в него добавляем список событий с событием 'click'(одинарное нажатие кнопки мыши) и запускаем функцию clickHandler
 
-function clickHandler () {
-  const li = listContainer.insertAdjacentHTML('beforeend', template); // создаём элемент <li>
-  
-  document.getElementById('listContainer').append(li) //получаем список ul с id="list" и добавляем в этот ul нашу лишку с текстом
-  textTask.textContent = inputText.value;
-  inputText.value = ' ';
-  return li;
+function createNewTask () {
+  if (inputText.value !== '') {
+    let content = template.replace('$CONTENT$', inputText.value);
+    listContainer.insertAdjacentHTML('beforeend', content); 
+    inputText.value = '';
+  }
 }
-
-function clickHandler () {
-  const createString = document.createElement('template'); // создаём элемент <li>
-}
-
-function createNewTask () { // создание новой задачи
-
-}
-
-function isValueValid () {
-
-} // Валидация вводимого значения на пустую строку
-
