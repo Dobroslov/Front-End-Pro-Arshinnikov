2138e2738f469e07cb6f904feded10c0f97fb5a0
Vladimir <78982910+Dobroslov@users.noreply.github.com>
2021-06-10 22:48:06 +0300

    remade data functions

--- DZ_13_to_do_list_PRO/script.js
+++ DZ_13_to_do_list_PRO/script.js
@@ -1,6 +1,11 @@
+const DELETED_BTN_CLASS = 'delete-btn';
+const TEXT_TASK_CLASS = 'text-task';
+
 const inputText = document.getElementById('input-task');
 const templateTask = document.getElementById('task-template').innerHTML; // innerHTML позволяет получить HTML-содержимое элемента в виде строки.
 const taskList = document.getElementById('task-list');
+
+const userTaskList = [];
 
 document
   .getElementById('add-task-btn')
@@ -10,36 +15,53 @@ taskList.addEventListener('click', onTaskListElementClick);
 
 function onAddNewTaskBtn() {
   if (isInputValue(inputText.value)) {
-    // console.log("clicked", inputText.value);
-
-    addNewTask(inputText.value);
+    const newTask = getFormTaskDate();
+    addTask(newTask);
     resetInput();
   }
 }
 
-function addNewTask(value) {
-  const NewTemplateTask = getNewTemplateTask(value);
-
-  taskList.insertAdjacentHTML('beforeend', NewTemplateTask);
-  // console.log('addNewTask', value);
+function addTask(task) {
+  userTaskList.push(task); // когда приходить новая задача, она добавляется в массив списка задач
+  // saveToStorage();
+  renderAllTasks(userTaskList); //функция которая создаёт все контакты из массива с последними изменениями
 }
 
-function getNewTemplateTask(value) {
-  // console.log('addtemplate', value);
-  return templateTask.replace('$content$', value);
+function renderAllTasks(arrListTask) {
+  taskList.innerHTML = ''; // делаю шаблон пустым
+  arrListTask.forEach((element) => {
+    renderTask(element); // массив с объектами задач перебираю через forEach и наполняю каждые элемент через шаблон
+  });
+}
+
+function renderTask(task) {
+  // функция визуализации контактов в DOM дереве
+  const NewTemplateTask = getNewTemplateTask(task);
+
+  taskList.insertAdjacentHTML('beforeend', NewTemplateTask);
 }
 
 function onTaskListElementClick(event) {
+  // функция по выполнению которой меняется выделение выбранной задачи на выполненную
   // event - это объетк события, в котором много разной информации о событии, в том числе и его место и его значение
-  if (event.target.classList.contains('text-task')) {
+  if (event.target.classList.contains(TEXT_TASK_CLASS)) {
     // у цели события через classList проверяю есть ли такой класс у элемента как (task-string), тут это класс строки с задачей, если нет, то пропуск, если есть, то меняю цвет
     toggleListElement(event.target); // запускаю функцию смены класса и в неё передаю таргет(объект или конкретный элемент, на котором это событие произошло)
+  }
+  if (event.target.classList.contains(DELETED_BTN_CLASS)) {
+    deleteListElement(event.target.closest('.' + TEXT_TASK_CLASS)); // запуск функции, которая удалить элемент в неё передаю селектор ".delete-btn"
   }
 }
 
 function toggleListElement(element) {
-  // переключаем класс(цвет) на элементе списка
+  // переключаем (добавляем или убираем 'done') класс(цвет) на элементе списка
   element.classList.toggle('done');
+}
+
+function deleteListElement(element) {
+  // функция удаления строки задачи(шаблона). Сюда приходит через element - event.target
+
+  element.remove();
 }
 
 function isInputValue(text) {
@@ -50,17 +72,33 @@ function resetInput() {
   inputText.value = '';
 }
 
+function getNewTemplateTask(value) {
+  return templateTask
+    .replace('$content$', value.taskContent)
+    .replace('$id$', value.id);
+}
+
+function getFormTaskDate() {
+  // шаблон для данных контакта, тут я формирую содержимое данных
+  return {
+    id: Date.now(),
+    taskContent: inputText.value,
+  };
+}
+
 init();
 
 function init() {
-  addNewTask('Это задача 1');
-  addNewTask('Это задача 2');
-  addNewTask('Это задача 3');
+  addTask({
+    id: Date.now(), //создаётся рандомное число, опираясь на текущее время
+    taskContent: 'Это задача 1',
+  });
+  addTask({
+    id: Date.now(),
+    taskContent: 'Это задача 2',
+  });
+  addTask({
+    id: Date.now(),
+    taskContent: 'Это задача 3',
+  });
 }
-
-function addEventForDeleteBtn(index) {}
-function onDeleteUserBtnClick(event) {}
-
-// let target = event.target; // получаем ссылку на ДОМ элемент на котором произошло событие
-// let targetRow = target.closest('.user-information'); // метод closest возвращает ближайший родительский элемент (или сам элемент), который соответствует заданному CSS-селектору (.user-information)
-// targetRow.remove();
