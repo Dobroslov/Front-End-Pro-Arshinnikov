0d71cc725db67b9eecdeff503caf9a857c40a099
Vladimir <78982910+Dobroslov@users.noreply.github.com>
2021-08-01 17:29:09 +0300

    Delete VLADLEN LESSON directory

--- VLADLEN LESSON/Function-zamikanie.js
+++
@@ -1,9 +0,0 @@
-// Функция замыкания - это когда из одной функции мы возвращаем другую функцию с сохранением некоторого контекста
-function craateMember(name) {
-  return function (lastName) {
-    console.log(name + lastName);
-  };
-}
-const lorWithLastName = craateMember('Владимир');
-console.log(lorWithLastName(' Аршинников'));
-console.log(lorWithLastName(' Нечипоренко'));
