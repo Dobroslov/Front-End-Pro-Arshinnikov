4d71dcd08aa5930268288fe1e4d0cdff78bddb5e
Vladimir <78982910+Dobroslov@users.noreply.github.com>
2021-08-16 12:48:57 +0300

    end dz, fix some functions

--- DZ_27_Chat/src/scripts/script.js
+++ DZ_27_Chat/src/scripts/script.js
@@ -5,30 +5,45 @@ const inputText = document.getElementById('input-text');
 const submitBtn = document.getElementById('submit-btn');
 const listMessages = document.getElementById('window-list-messages');
 
-submitBtn.addEventListener('click', onBtnSubmitNewMessage);
-
 function initConnection() {
   let socket = new WebSocket(url);
-
+  submitBtn.addEventListener('submit', onBtnSubmitNewMessage);
   socket.onopen = function () {
     // проверка открылось ли соединение
+    const startMessege = (message = {
+      type: 'userLogIn',
+      payload: { user: 'Новый пользователь', message: 'присоединился к чату' },
+    });
     console.log('[open] Соединение установлено');
+    sendMessage(startMessege);
     console.log('Отправляем данные на сервер');
   };
 
   socket.onmessage = (msg) => {
     console.log(`[message] Данные получены с сервера: ${msg.data}`, msg);
     const incomingMessage = JSON.parse(msg.data);
+    console.log('🚀 ~ initConnection ~ incomingMessage', incomingMessage);
+
     renderMessage(incomingMessage);
   };
 
   socket.onclose = function (event) {
     // закрытие соединения
     if (event.wasClean) {
+      const exitMessage = (message = {
+        type: 'userLogOut',
+        payload: { user: 'Пользователь', message: 'вышел из чата' },
+      });
+      renderMessage(exitMessage);
       console.log(
         `[close] Соединение закрыто чисто, код=${event.code} причина=${event.reason}`
       );
     } else {
+      const exitMessage = (message = {
+        type: 'userLogOut',
+        payload: { user: 'Пользователь', message: 'вышел из чата' },
+      });
+      renderMessage(exitMessage);
       console.log(`[close] Соединение прервано`);
       initConnection();
     }
@@ -37,44 +52,44 @@ function initConnection() {
   socket.onerror = function (err) {
     console.log(`[error] ${err.message}`);
   };
-}
 
-initConnection();
-
-function onBtnSubmitNewMessage() {
-  const newMessage = getFormDate();
-  if (isInputValue(inputName.value, inputText.value)) {
-    sendMessage(newMessage);
-    renderMessage(newMessage);
-    resetFormMessage();
+  function sendMessage(message) {
+    if (socket.readyState === WebSocket.OPEN)
+      socket.send(JSON.stringify(message));
   }
-}
 
-function isInputValue(user, message) {
-  return user.trim() !== '' && message.trim() !== '';
-}
+  function onBtnSubmitNewMessage() {
+    const newMessage = getFormDate();
+    if (isInputValue(inputName.value, inputText.value)) {
+      sendMessage(newMessage);
+      resetFormMessage();
+    }
+  }
 
-function getFormDate() {
-  return (message = {
-    type: 'userMessage',
-    payload: { user: inputName.value, message: inputText.value },
-  });
-}
+  function isInputValue(user, message) {
+    return user.trim() !== '' && message.trim() !== '';
+  }
 
-function renderMessage(newMessageObj) {
-  return listMessages.insertAdjacentHTML(
-    'beforeend',
-    `
+  function getFormDate() {
+    return (message = {
+      type: 'userMessage',
+      payload: { user: inputName.value, message: inputText.value },
+    });
+  }
+
+  function renderMessage(newMessageObj) {
+    return listMessages.insertAdjacentHTML(
+      'beforeend',
+      `
     <p>
       ${newMessageObj.payload.user}: <span class="text">${newMessageObj.payload.message}</span>
     </p>`
-  );
+    );
+  }
+
+  function resetFormMessage() {
+    inputText.value = '';
+  }
 }
 
-function resetFormMessage() {
-  inputText.value = '';
-}
-
-function sendMessage(message) {
-  socket.send(JSON.stringify(message));
-}
+initConnection();
